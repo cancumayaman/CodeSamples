3e24273fdba026f67d27e70b7820580311744f6c
Can Cuma Yaman <Can.Yaman@d-teknoloji.com.tr>
2023-05-28 17:06:45 +0300

    Compare Async Task and Async Void for error handling

---
+++ AsyncTaskAndAsyncVoid/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AsyncTaskAndAsyncVoid
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                await AsyncTaskMethod();
+                //AsyncVoidMethod();
+                Console.WriteLine("Hello, World!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static async Task AsyncTaskMethod()
+        {
+            await Task.Delay(1000);
+            throw new Exception("An Error Occured");
+        }
+        public static async void AsyncVoidMethod()
+        {
+            await Task.Delay(1000);
+            throw new Exception("An Error Occured");
+        }
+    }
+}
